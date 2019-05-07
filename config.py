12cc6760eb0d7d1b145a0efd3dca793e66c09f94
Alexander Robinson <robinson@fis.ucm.es>
2019-05-07 15:45:21 +0200

    Simplified config.py to specify the path of the config file directly, instead of using an automatically generated name.

--- config.py
+++ config.py
@@ -10,34 +10,25 @@ import sys
 # Manage command-line arguments to load compiler option
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    'compiler', metavar='COMPILER', type=str, choices=['ifort','gfortran'],
-     help='Compiler choice should be included as an argument: ifort or gfortran')
+    'config', metavar='CONFIG', type=str,
+     help='Name of config file to use (e.g., config/aire_gfortran)')
 args = parser.parse_args()
 
 
 # Determine locations
-config_dir = "config/"
-target_dir = "./"
-compiler   = args.compiler 
-
-# Determine the current hostname
-proc = Popen("hostname",shell=True,stdout=PIPE,stderr=PIPE)
-host = proc.communicate()[0].strip().decode("utf-8")
-
-if host == "manager.cei.ucm.local": host = "eolo"
-if host in ["login01","login02"]:   host = "pik"
-
-print("{}{}_{}".format(config_dir,host,compiler))
+target_dir  = "./"
+config_dir  = "config/"
+config_path = args.config 
 
 # Load template Makefile and insert compiler configuration
 makefile0    = open(config_dir+"Makefile").read()
-compile_info = open(config_dir+host+"_"+compiler).read()
+compile_info = open(config_path).read()
 makefile1 = makefile0.replace("<COMPILER_CONFIGURATION>",compile_info)
 
 # Write the new Makefile to the main directory
 open(target_dir+"Makefile","w").write(makefile1)
 
-print( "".join(["\n\nMakefile configuration complete for host - compiler: ",host," - ",compiler,"\n"]) )
+print( "".join(["\n\nMakefile configuration complete for configuration file: ",config_path,"\n"]) )
 
 instructions = '''==== How to run yelmo ====\n
 # Make a link to the ice_data path, if available.
@@ -57,4 +48,16 @@ ncview yelmo2D.nc
 
 print(instructions)
 
+
+
+### Old script commands to automatically determine the config_path from host + compiler:
+
+# Determine the current hostname
+#proc = Popen("hostname",shell=True,stdout=PIPE,stderr=PIPE)
+#host = proc.communicate()[0].strip().decode("utf-8")
+
+#if host == "manager.cei.ucm.local": host = "eolo"
+#if host in ["login01","login02"]:   host = "pik"
+
+#print("{}{}_{}".format(config_dir,host,compiler))
 

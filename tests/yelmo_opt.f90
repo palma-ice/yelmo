84d9da88da6e2bb4319ace6a34a24f63afdcb815
Alexander Robinson <robinson@fis.ucm.es>
2019-06-21 16:48:05 -0600

    opt: Increased range of phi_min/phi_max.

--- tests/yelmo_opt.f90
+++ tests/yelmo_opt.f90
@@ -62,11 +62,11 @@ program yelmo_test
     ! Simulation parameters
     time_init           = 0.0       ! [yr] Starting time
     time_iter           = 500.0     ! [yr] Simulation time for each iteration
-    qmax                = 101       ! Total number of iterations
+    qmax                = 51        ! Total number of iterations
     qmax_topo_fixed     = 0         ! Number of initial iterations that should use topo_fixed=.TRUE. 
     qmax_iter_length    = 10        ! Number of iterations at which iteration length should increase
     phi_min             =  5.0      ! Minimum allowed friction angle
-    phi_max             = 40.0      ! Maximum allowed friction angle 
+    phi_max             = 70.0      ! Maximum allowed friction angle 
 
     ! Prescribe key parameters here that should be set for beta optimization exercise 
     yelmo1%dyn%par%C_bed_method      = -1       ! C_Bed is set external to yelmo calculations

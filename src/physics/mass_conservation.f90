520e5549f1b71ee0a7eaa65e8bcb992f7647a786
Alexander Robinson <robinson@fis.ucm.es>
2019-06-24 15:14:14 -0600

    Trying new H_ref calculation for ice margin (H_ref=H_upstream), with no reduction for grounded ice.

--- src/physics/mass_conservation.f90
+++ src/physics/mass_conservation.f90
@@ -891,12 +891,12 @@ end if
 
                     end if 
 
-                    if (f_grnd(i,j) .gt. 0.0) then 
-                        ! For grounded ice, set H_ref < H_neighb arbitrarily (0.5 works well)
+!                     if (f_grnd(i,j) .gt. 0.0) then 
+!                         ! For grounded ice, set H_ref < H_neighb arbitrarily (0.5 works well)
 
-                        H_ref = 0.5*H_ref 
+!                         H_ref = 0.5*H_ref 
 
-                    end if 
+!                     end if 
 
 !                     ! Determine height to give to partially filled cell
 !                     if (f_grnd(i,j) .eq. 0.0) then 

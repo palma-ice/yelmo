e7b30d323add49d2724ce1b9957795a8b688d7ce
Alexander Robinson <robinson@fis.ucm.es>
2019-09-26 12:32:48 +0200

    Starting retuning of C_bed.

--- tests/yelmo_initmip.f90
+++ tests/yelmo_initmip.f90
@@ -494,7 +494,7 @@ contains
                     ! Default
                     lambda_bed = calc_lambda_bed_exp(bnd%z_bed,dyn%par%cb_z0,dyn%par%cb_z1)
 
-if (.TRUE.) then                     
+if (.FALSE.) then                     
                     ! Modifications 
                     where (bnd%basins .eq. 1) lambda_bed = calc_lambda_bed_exp(bnd%z_bed,-500.0,dyn%par%cb_z1)
                     where (bnd%basins .eq. 2) lambda_bed = calc_lambda_bed_exp(bnd%z_bed,-300.0,dyn%par%cb_z1)
@@ -531,7 +531,7 @@ end if
             ! Additionally modify cf_ref
             if (trim(domain) .eq. "Antarctica") then
 
-if (.TRUE.) then
+if (.FALSE.) then
                 ! Reduction 
                 call scale_cb_gaussian(cf_ref,dyn%par%cf_stream*0.25,x0=-2000.0,y0=1000.0, sigma=100.0,xx=grd%x*1e-3,yy=grd%y*1e-3)
                 call scale_cb_gaussian(cf_ref,dyn%par%cf_stream*0.20,x0=-750.0, y0=-900.0, sigma=300.0,xx=grd%x*1e-3,yy=grd%y*1e-3)

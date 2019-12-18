3a30dd443c9575c594a8d3670d3369e1216d1bc9
Alexander Robinson <robinson@fis.ucm.es>
2019-12-18 10:57:53 +0100

    Disabled gaussian smoothing of margin ice thickness for temperature calculations (seems to affect benchmark results).

--- src/yelmo_thermodynamics.f90
+++ src/yelmo_thermodynamics.f90
@@ -233,7 +233,7 @@ contains
         ! Local variables
         integer :: i, j, k, nx, ny, nz_aa, nz_ac  
         real(prec), allocatable  :: advecxy(:)   ! [K a-1 m-2] Horizontal heat advection 
-        real(prec), allocatable  :: uz_corr(:)   ! [m a-1] Corrected vertical velocity 
+        real(prec), allocatable  :: uz_now(:)   ! [m a-1] Corrected vertical velocity 
         real(prec) :: T_shlf, H_grnd_lim, f_scalar, T_base  
         real(prec) :: H_ice_now 
 
@@ -254,7 +254,7 @@ contains
         allocate(enth_old(nx,ny,nz_aa))
         allocate(advecxy3D(nx,ny,nz_aa))
         
-        allocate(uz_corr(nz_ac))
+        allocate(uz_now(nz_ac))
 
         ! First perform horizontal advection (this doesn't work properly, 
         ! use column-based upwind horizontal advection below)
@@ -324,6 +324,7 @@ contains
                 ! H_ice_now = sum(H_ice(i-1:i+1,j-1:j+1)*filter)
                 
                 ! Filter at the margin only 
+if (.FALSE.) then 
                 if (count(H_ice(i-1:i+1,j-1:j+1) .eq. 0.0) .ge. 2) then
                     filter = filter0 
                     where (H_ice(i-1:i+1,j-1:j+1) .eq. 0.0) filter = 0.0 
@@ -332,10 +333,10 @@ contains
                 else 
                     H_ice_now = H_ice(i,j) 
                 end if 
-                
+end if 
                 ! Pre-calculate the contribution of horizontal advection to column solution
                 ! (use unmodified T_ice_old field as input, to avoid mixing with new solution)
-                call calc_advec_horizontal_column(advecxy,T_ice_old,H_ice,z_srf,ux,uy,zeta_aa,dx,i,j)
+!                 call calc_advec_horizontal_column(advecxy,T_ice_old,H_ice,z_srf,ux,uy,zeta_aa,dx,i,j)
 !                 call calc_advec_horizontal_column_quick(advecxy,T_ice_old,H_ice,ux,uy,dx,i,j)
 !                 do k = 1, nz_aa
 !                     call calc_adv2D_expl_rate(advecxy(k),T_ice_old(:,:,k),ux(:,:,k),uy(:,:,k),dx,dx,i,j)
@@ -344,17 +345,16 @@ contains
                 !advecxy = 0.0_prec 
 !                 write(*,*) "advecxy: ", i,j, maxval(abs(advecxy3D(i,j,:)-advecxy))
 
-!                 call calc_advec_horizontal_column(advecxy,enth_old,H_ice,z_srf,ux,uy,zeta_aa,dx,i,j)
+                call calc_advec_horizontal_column(advecxy,enth_old,H_ice,z_srf,ux,uy,zeta_aa,dx,i,j)
 
-
-                call calc_advec_vertical_column_correction(uz_corr,H_ice,z_srf,ux,uy,uz,zeta_ac,dx,i,j)
+                call calc_advec_vertical_column_correction(uz_now,H_ice,z_srf,ux,uy,uz,zeta_ac,dx,i,j)
 
 !                 do k = 1, nz_aa
 !                     call calc_adv2D_expl_rate(advecxy(k),enth_old(:,:,k),ux(:,:,k),uy(:,:,k),dx,dx,i,j)
 !                 end do 
 
                 call calc_enth_column(enth(i,j,:),T_ice(i,j,:),omega(i,j,:),bmb_grnd(i,j),Q_ice_b(i,j),H_cts(i,j), &
-                        T_pmp(i,j,:),cp(i,j,:),kt(i,j,:),advecxy,uz_corr,Q_strn(i,j,:),Q_b(i,j),Q_geo(i,j),T_srf(i,j), &
+                        T_pmp(i,j,:),cp(i,j,:),kt(i,j,:),advecxy,uz_now,Q_strn(i,j,:),Q_b(i,j),Q_geo(i,j),T_srf(i,j), &
                         T_shlf,H_ice_now,H_w(i,j),f_grnd(i,j),zeta_aa,zeta_ac,dzeta_a,dzeta_b,cr,omega_max,T0,dt)
                 
             end if 

e0bcf033a30605a5dc638aa6006e35c61817fdaf
Alexander Robinson <robinson@fis.ucm.es>
2020-03-04 11:03:02 +0100

    Bug fix in mismip exe.

--- tests/yelmo_mismip.f90
+++ tests/yelmo_mismip.f90
@@ -5,7 +5,6 @@ program yelmo_mismip
     use ncio 
     use yelmo 
     use yelmo_tools, only : stagger_aa_acx, stagger_aa_acy
-    use yelmo_dynamics, only: check_vel_convergence
     use deformation 
 
     use mismip3D 
@@ -309,6 +308,10 @@ contains
 
         ! Write model speed 
         call nc_write(filename,"speed",ylmo%par%model_speed,units="kyr/hr",long_name="Model speed (Yelmo only)", &
+                      dim1="time",start=[n],count=[1],ncid=ncid)
+        call nc_write(filename,"dt_avg",ylmo%par%dt_avg,units="yr",long_name="Average timestep", &
+                      dim1="time",start=[n],count=[1],ncid=ncid)
+        call nc_write(filename,"eta_avg",ylmo%par%eta_avg,units="m a-1",long_name="Average eta (maximum PC truncation error)", &
                       dim1="time",start=[n],count=[1],ncid=ncid)
         
         ! 1D variables of interest 

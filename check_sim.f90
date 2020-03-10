cabbdbdba43cec18d72108ee9891321a377a7b16
Alexander Robinson <robinson@fis.ucm.es>
2020-03-10 17:31:15 +0100

    Improved check_sim to include folder name.

--- check_sim.f90
+++ check_sim.f90
@@ -1,5 +1,6 @@
 program check_sim 
-    
+    ! for D in tmp/yelmo991/ant-pd-ens1/* ; do ./check_sim.x $D ; done
+
     use ncio 
 
     implicit none 
@@ -19,6 +20,7 @@ program check_sim
     integer :: narg, n
     character(len=256) :: fldr_path, file_path  
     real(wp) :: time, rmse_H, rmse_uxy, rmse_uxy_log 
+    character(len=256) :: fldr_path_root, fldr_sim 
 
     ! Get command line arguments 
 
@@ -31,6 +33,9 @@ program check_sim
 
     call get_command_argument(1,fldr_path)
 
+    ! Determine just the simulation folder for output purposes 
+    call split_string(fldr_path,"/",fldr_path_root,fldr_sim,back=.TRUE.)
+
     file_path = trim(fldr_path)//"/yelmo2D.nc" 
 
     n = nc_size(file_path,"time")
@@ -40,9 +45,33 @@ program check_sim
     call nc_read(file_path,"rmse_uxy",    rmse_uxy,    start=[n],count=[1])
     call nc_read(file_path,"rmse_uxy_log",rmse_uxy_log,start=[n],count=[1])
     
-    write(*,*) time, rmse_H, rmse_uxy, rmse_uxy_log 
-    
+    write(*,*) trim(fldr_sim), time, rmse_H, rmse_uxy, rmse_uxy_log 
+
 contains 
 
+    subroutine split_string(instring,delim,string1,string2,back)
+        ! split a string into 2 either side of a delimiter token
+
+        character(*), intent(IN)  :: instring
+        character(*), intent(IN)  :: delim 
+        character(*), intent(OUT) :: string1
+        character(*), intent(OUT) :: string2
+        logical,      intent(IN), optional :: back 
+
+        ! Local variables
+        integer :: index
+        character(len=len(instring)) :: string0
+
+        string0 = trim(adjustl(instring))
+
+        index   = scan(string0,delim,back=back)
+        if (string0(index+1:index+1) .eq. " ") string0(index:index) = " "  ! Remove trailing character
+        index   = scan(string0,delim,back=back)
+        string1 = string0(1:index-1)
+        string2 = string0(index+1:)
+
+        return 
+
+    end subroutine split_string
 
 end program check_sim 

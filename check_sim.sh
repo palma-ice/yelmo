8fd5c9097097f04c1a4c792bfc05039c4e2e7c34
Alexander Robinson <robinson@fis.ucm.es>
2020-03-11 11:05:28 +0100

    Improved check_sim.sh.

--- check_sim.sh
+++ check_sim.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
 fldr=$1
+file_out=${fldr}/ens_err.txt 
 
-for D in ${fldr}/* ; do ./check_sim.x $D ; done > ${fldr}/ens_err.txt
+echo "Processing results in: ${fldr}"
+
+#for D in ${fldr}/* ; do ./check_sim.x $D ; done > ${file_out}
+
+# Initialize file with table header
+echo 'sim   time     rmse_H      rmse_uxy     rmse_uxy_log' > ${file_out} 
+
+# Loop over subfolders in fldr 
+for D in ${fldr}/*
+do
+    if [[ -d "$D" ]]
+    then
+        echo $D 
+        ./check_sim.x $D >> ${file_out}
+    fi
+done 
+
+echo "Results written to: ${file_out}"

cf8c8b045cdb1f6e2ce08f0f9fb4e0ff48c86479
Alexander Robinson <robinson@ucm.es>
2021-11-06 21:57:50 +0100

    Reactivated possible bmb_grnd values for floating ice due to internal melting.

--- src/physics/ice_enthalpy.f90
+++ src/physics/ice_enthalpy.f90
@@ -232,7 +232,7 @@ contains
         bmb_grnd = bmb_grnd - melt_internal 
 
         ! ajr: testing
-        if (f_grnd .eq. 0.0) bmb_grnd = 0.0_wp 
+        !if (f_grnd .eq. 0.0) bmb_grnd = 0.0_wp 
 
         ! Safety: limit bmb_grnd to reasonable values to avoid problems
         ! (grounded ice melt should be much less than this limit, eg 10 m/yr)

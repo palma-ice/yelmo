867780e043c6e94fad541a62b1459708e8961356
Alexander Robinson <robinson@ucm.es>
2021-11-13 16:57:00 +0100

    ssa solver: added preprocessing section for testing new derivation of lateral boundary conditions (currently populated with NEWCODE version).

--- src/physics/solver_ssa_sico5.F90
+++ src/physics/solver_ssa_sico5.F90
@@ -85,7 +85,10 @@ contains
         real(wp) :: tau_bc_int 
         real(wp) :: tau_bc_sign
 
-!#define OLDCODE 
+! Only one at a time!!
+!#define LAT_BC_OLDCODE
+!#define LAT_BC_NEWCODE
+#define LAT_BC_NEWCODE1       
 
 ! Include header for lis solver fortran interface
 #include "lisf.h"
@@ -100,12 +103,6 @@ contains
         LIS_INTEGER :: nmax, n_sprs 
         LIS_INTEGER, allocatable, dimension(:) :: lgs_a_ptr, lgs_a_index
         LIS_SCALAR,  allocatable, dimension(:) :: lgs_a_value, lgs_b_value, lgs_x_value
-
-! #if (defined(OLDCODE))
-!         write(*,*) "ssa: OLDCODE lateral BCs!"
-! #else
-!         write(*,*) "ssa: NEWCODE lateral BCs!"
-! #endif
 
         ! Define border conditions (zeros, infinite, periodic)
         select case(trim(boundaries)) 
@@ -525,7 +522,7 @@ contains
 
                 end select 
 
-#if (defined(OLDCODE))
+#if (defined(LAT_BC_OLDCODE))
             ! ===== OLDCODE lateral BCs =====
 
             else if (  ( is_front_1(i,j).and.is_front_2(i+1,j) ) &
@@ -611,8 +608,10 @@ contains
                         lgs_b_value(nr) = 0.0_prec
                         lgs_x_value(nr) = 0.0_prec
 
-                    end if 
-#else
+                    end if
+
+#endif 
+#if (defined(LAT_BC_NEWCODE))
             ! ===== NEWCODE lateral BCs =====
 
             else if (  ( is_front_1(i,j).and.is_front_2(i+1,j) ) &
@@ -738,7 +737,132 @@ contains
                     end if
 
 
+#endif 
+#if (defined(LAT_BC_NEWCODE1))
+                    ! === NEWCODE1 ====================================
 
+            else if (  ( is_front_1(i,j).and.is_front_2(i+1,j) ) &
+                      .or. &
+                      ( is_front_2(i,j).and.is_front_1(i+1,j) ) &
+                    ) then
+                    ! one neighbour is ice-covered and the other is ice-free
+                    ! (calving front, grounded ice front)
+
+                    if (is_front_1(i,j)) then
+                        i1 = i     ! ice-front marker 
+                    else   ! is_front_1(i+1,j)==.true.
+                        i1 = i+1   ! ice-front marker 
+                    end if
+                    
+                    if ( (.not. is_front_2(i1-1,j)) .or. (.not. is_front_2(i1+1,j)) ) then
+                        ! There is inland ice on one side of the current cell, proceed
+                        ! with calving front boundary conditions 
+
+                        ! =========================================================
+                        ! Generalized solution for all ice fronts (floating and grounded)
+                        ! See Lipscomb et al. (2019), Eqs. 11 & 12, and 
+                        ! Winkelmann et al. (2011), Eq. 27 
+
+                        ! Get current ice thickness
+                        ! (No f_ice scaling since all points treated have f_ice=0/1)
+                        H_ice_now = H_ice(i1,j)     
+
+                        ! Get current ocean thickness bordering ice sheet
+                        ! (for bedrock above sea level, this will give zero)
+                        f_submerged = 1.d0 - min((z_srf(i1,j)-z_sl(i1,j))/H_ice_now,1.d0)
+                        H_ocn_now   = H_ice_now*f_submerged
+                        
+                        tau_bc_int = 0.5d0*rho_ice*g*H_ice_now**2 &         ! tau_out_int                                                ! p_out
+                                   - 0.5d0*rho_sw *g*H_ocn_now**2           ! tau_in_int
+
+                        ! =========================================================
+                        
+                        if (is_front_1(i,j).and.is_front_2(i+1,j)) then 
+                            ! === Case 1: ice-free to the right ===
+
+                            nc = 2*ij2n(i-1,j)-1
+                                ! smallest nc (column counter), for vx_m(i-1,j)
+                            k = k+1
+                            lgs_a_value(k) = -4.0_prec*inv_dxi*vis_int_g(i1,j)
+                            lgs_a_index(k) = nc 
+
+                            nc = 2*ij2n(i,j-1)
+                                ! next nc (column counter), for vy_m(i,j-1)
+                            k = k+1
+                            lgs_a_value(k) = -2.0_prec*inv_deta*vis_int_g(i1,j)
+                            lgs_a_index(k) = nc
+
+                            nc = 2*ij2n(i,j)-1
+                                ! next nc (column counter), for vx_m(i,j)
+        !                     if (nc /= nr) then   ! (diagonal element)
+        !                         errormsg = ' >>> calc_vxy_ssa_matrix: ' &
+        !                                      //'Check for diagonal element failed!'
+        !                         call error(errormsg)
+        !                     end if
+                            k = k+1
+                            lgs_a_value(k) = 4.0_prec*inv_dxi*vis_int_g(i1,j)
+                            lgs_a_index(k) = nc
+
+                            nc = 2*ij2n(i,j)
+                                ! next nc (column counter), for vy_m(i,j)
+                            k = k+1
+                            lgs_a_value(k) = 2.0_prec*inv_deta*vis_int_g(i1,j)
+                            lgs_a_index(k) = nc
+
+                            ! Assign matrix values
+                            lgs_b_value(nr) = tau_bc_int
+                            lgs_x_value(nr) = vx_m(i,j)
+                        
+                        else 
+                            ! Case 2: ice-free to the left
+ 
+                            nc = 2*ij2n(i,j)-1
+                                ! next nc (column counter), for vx_m(i,j)
+        !                     if (nc /= nr) then   ! (diagonal element)
+        !                         errormsg = ' >>> calc_vxy_ssa_matrix: ' &
+        !                                      //'Check for diagonal element failed!'
+        !                         call error(errormsg)
+        !                     end if
+                            k = k+1
+                            lgs_a_value(k) = -4.0_prec*inv_dxi*vis_int_g(i1,j)
+                            lgs_a_index(k) = nc
+
+                            nc = 2*ij2n(i+1,j-1)
+                                ! next nc (column counter), for vy_m(i+1,j-1)
+                            k  = k+1
+                            lgs_a_value(k) = -2.0_prec*inv_deta*vis_int_g(i1,j)
+                            lgs_a_index(k) = nc
+
+                            nc = 2*ij2n(i+1,j)-1
+                                ! next nc (column counter), for vx_m(i+1,j)
+                            k = k+1
+                            lgs_a_value(k) = 4.0_prec*inv_dxi*vis_int_g(i1,j)
+                            lgs_a_index(k) = nc
+ 
+                            nc = 2*ij2n(i+1,j)
+                                ! largest nc (column counter), for vy_m(i+1,j)
+                            k  = k+1
+                            lgs_a_value(k) = 2.0_prec*inv_deta*vis_int_g(i1,j)
+                            lgs_a_index(k) = nc
+
+                            ! Assign matrix values
+                            lgs_b_value(nr) = tau_bc_int
+                            lgs_x_value(nr) = vx_m(i,j)
+                        
+                        end if 
+
+                    else    ! (is_front_2(i1-1,j)==.true.).and.(is_front_2(i1+1,j)==.true.);
+                            ! velocity assumed to be zero
+
+                        k = k+1
+                        lgs_a_value(k)  = 1.0_prec   ! diagonal element only
+                        lgs_a_index(k)  = nr
+
+                        lgs_b_value(nr) = 0.0_prec
+                        lgs_x_value(nr) = 0.0_prec
+
+                    end if
+                    
 #endif
 
             else if ( ( (maske(i,j)==3).and.(maske(i+1,j)==1) ) &
@@ -1076,7 +1200,7 @@ contains
 
                 end select 
 
-#if (defined(OLDCODE))
+#if (defined(LAT_BC_OLDCODE))
             ! ===== OLDCODE lateral BCs =====
 
             else if (  ( is_front_1(i,j).and.is_front_2(i,j+1) ) &
@@ -1161,7 +1285,9 @@ contains
                         lgs_x_value(nr) = 0.0_prec
 
                     end if
-#else
+
+#endif 
+#if (defined(LAT_BC_NEWCODE))
             ! ===== NEWCODE lateral BCs =====
 
             else if (  ( is_front_1(i,j).and.is_front_2(i,j+1) ) &
@@ -1285,6 +1411,137 @@ contains
                         lgs_x_value(nr) = 0.0_prec
 
                     end if
+
+#endif 
+#if (defined(LAT_BC_NEWCODE1))
+                    ! === NEWCODE1 ====================================
+
+
+
+            else if (  ( is_front_1(i,j).and.is_front_2(i,j+1) ) &
+                      .or. &
+                      ( is_front_2(i,j).and.is_front_1(i,j+1) ) &
+                    ) then
+                    ! one neighbour is ice-covered and the other is ice-free
+                    ! (calving front, grounded ice front)
+
+                    if (is_front_1(i,j)) then
+                        j1 = j     ! ice-front marker
+                    else   ! is_front_1(i,j+1)==.true.
+                        j1 = j+1   ! ice-front marker
+                    end if
+
+                    if ( (.not. is_front_2(i,j1-1)) .or. (.not. is_front_2(i,j1+1)) ) then
+                        ! There is inland ice on one side of the current cell, proceed
+                        ! with calving front boundary conditions 
+
+                        ! =========================================================
+                        ! Generalized solution for all ice fronts (floating and grounded)
+                        ! See Lipscomb et al. (2019), Eqs. 11 & 12, and 
+                        ! Winkelmann et al. (2011), Eq. 27 
+
+                        ! Get current ice thickness
+                        ! (No f_ice scaling since all points treated have f_ice=0/1)
+                        H_ice_now = H_ice(i,j1)     
+
+                        ! Get current ocean thickness bordering ice sheet
+                        ! (for bedrock above sea level, this will give zero)
+                        f_submerged = 1.d0 - min((z_srf(i,j1)-z_sl(i,j1))/H_ice_now,1.d0)
+                        H_ocn_now   = H_ice_now*f_submerged
+
+                        tau_bc_int = 0.5d0*rho_ice*g*H_ice_now**2 &         ! tau_out_int                                                ! p_out
+                                   - 0.5d0*rho_sw *g*H_ocn_now**2           ! tau_in_int
+
+                        ! =========================================================
+                        
+                        if (is_front_1(i,j).and.is_front_2(i,j+1)) then 
+                            ! === Case 1: ice-free to the top ===
+
+                            nc = 2*ij2n(i-1,j)-1
+                                ! smallest nc (column counter), for vx_m(i-1,j)
+                            k = k+1
+                            lgs_a_value(k) = -2.0_prec*inv_dxi*vis_int_g(i,j1)
+                            lgs_a_index(k) = nc
+
+                            nc = 2*ij2n(i,j-1)
+                                ! next nc (column counter), for vy_m(i,j-1)
+                            k = k+1
+                            lgs_a_value(k) = -4.0_prec*inv_deta*vis_int_g(i,j1)
+                            lgs_a_index(k) = nc
+
+                            nc = 2*ij2n(i,j)-1
+                                ! next nc (column counter), for vx_m(i,j)
+                            k = k+1
+                            lgs_a_value(k) = 2.0_prec*inv_dxi*vis_int_g(i,j1)
+                            lgs_a_index(k) = nc
+
+                            nc = 2*ij2n(i,j)
+                                ! next nc (column counter), for vy_m(i,j)
+        !                     if (nc /= nr) then   ! (diagonal element)
+        !                         errormsg = ' >>> calc_vxy_ssa_matrix: ' &
+        !                                     //'Check for diagonal element failed!'
+        !                         call error(errormsg)
+        !                     end if
+                            k = k+1
+                            lgs_a_value(k) = 4.0_prec*inv_deta*vis_int_g(i,j1)
+                            lgs_a_index(k) = nc
+
+                            ! Assign matrix values
+                            lgs_b_value(nr) = tau_bc_int
+                            lgs_x_value(nr) = vy_m(i,j)
+                            
+                        else
+                            ! === Case 2: ice-free to the bottom ===
+ 
+                            nc = 2*ij2n(i-1,j+1)-1
+                                ! next nc (column counter), for vx_m(i-1,j+1)
+                            k = k+1
+                            lgs_a_value(k) = -2.0_prec*inv_dxi*vis_int_g(i,j1)
+                            lgs_a_index(k) = nc
+ 
+                            nc = 2*ij2n(i,j)
+                                ! next nc (column counter), for vy_m(i,j)
+        !                     if (nc /= nr) then   ! (diagonal element)
+        !                         errormsg = ' >>> calc_vxy_ssa_matrix: ' &
+        !                                     //'Check for diagonal element failed!'
+        !                         call error(errormsg)
+        !                     end if
+                            k = k+1
+                            lgs_a_value(k) = -4.0_prec*inv_deta*vis_int_g(i,j1)
+                            lgs_a_index(k) = nc
+ 
+                            nc = 2*ij2n(i,j+1)-1
+                                ! next nc (column counter), for vx_m(i,j+1)
+                            k = k+1
+                            lgs_a_value(k) = 2.0_prec*inv_dxi*vis_int_g(i,j1)
+                            lgs_a_index(k) = nc
+ 
+                            nc = 2*ij2n(i,j+1)
+                                ! next nc (column counter), for vy_m(i,j+1)
+                            k = k+1
+                            lgs_a_value(k) = 4.0_prec*inv_deta*vis_int_g(i,j1)
+                            lgs_a_index(k) = nc
+
+                            ! Assign matrix values
+                            lgs_b_value(nr) = tau_bc_int
+                            lgs_x_value(nr) = vy_m(i,j)
+                 
+                        end if 
+
+                    else    ! (is_front_2(i,j1-1)==.true.).and.(is_front_2(i,j1+1)==.true.);
+                            ! velocity assumed to be zero
+
+                        k = k+1
+                        lgs_a_value(k)  = 1.0_prec   ! diagonal element only
+                        lgs_a_index(k)  = nr
+
+                        lgs_b_value(nr) = 0.0_prec
+                        lgs_x_value(nr) = 0.0_prec
+
+                    end if
+
+
+
 
 #endif
             else if ( ( (maske(i,j)==3).and.(maske(i,j+1)==1) ) &

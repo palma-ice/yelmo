9a186a0c145a72571f9723c746fa86e57291212e
ilaria-tab <50909346+ilaria-tab@users.noreply.github.com>
2022-06-21 10:46:19 +0200

    Add till_cf_negis to exp law

--- src/yelmo_dynamics.f90
+++ src/yelmo_dynamics.f90
@@ -814,7 +814,8 @@ contains
                     lambda_bed = calc_lambda_bed_exp(bnd%z_bed,bnd%z_sl,dyn%par%till_z0,dyn%par%till_z1)
 
                     dyn%now%cb_ref = dyn%par%till_cf_ref * lambda_bed 
-                    where(dyn%now%cb_ref .lt. dyn%par%till_cf_min) dyn%now%cb_ref = dyn%par%till_cf_min 
+                    where(dyn%now%cb_ref .lt. dyn%par%till_cf_min) dyn%now%cb_ref = dyn%par%till_cf_min * dyn%par%till_cf_negis 
+                    where(bnd%basins .eq. 9) dyn%now%cb_ref = dyn%par%till_cf_ref * dyn%par%till_cf_negis * lambda_bed 
 
                 case DEFAULT
                     ! Scaling not recognized.

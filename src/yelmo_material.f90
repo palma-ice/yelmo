d2daba68f61a79387d23a7e565639c9212171c1d
Alexander Robinson <robinson@ucm.es>
2023-01-26 21:07:20 +0100

    Cleaned up deformation - removed unused strain rate routines, some of which seemed to produce asymmetries. Results now appear identical to revision 9015f4e (until 400 years into the trough simulation).

--- src/yelmo_material.f90
+++ src/yelmo_material.f90
@@ -85,25 +85,9 @@ contains
 
         ! Calculate the strain rate tensor
 
-if (.FALSE.) then 
-        ! Calculate strain rate tensor on ab-nodes, then average 
-
         call calc_strain_rate_tensor(mat%now%strn,mat%now%strn2D,dyn%now%ux,dyn%now%uy,dyn%now%uz, &
                                  tpo%now%H_ice,tpo%now%f_ice,tpo%now%f_grnd,mat%par%zeta_aa, &
                                  mat%par%zeta_ac,mat%par%dx,mat%par%dx,mat%par%de_max,mat%par%n_glen,tpo%par%boundaries)
-
-else 
-        ! Calculate strain rate tensor on aa-nodes directly
-
-        call calc_strain_rate_tensor_new_aa(mat%now%strn,mat%now%strn2D,dyn%now%ux,dyn%now%uy,dyn%now%uz, &
-                                 tpo%now%H_ice,tpo%now%f_ice,tpo%now%f_grnd,mat%par%zeta_aa, &
-                                 mat%par%zeta_ac,mat%par%dx,mat%par%de_max,mat%par%n_glen)
-
-        ! call calc_strain_rate_tensor_aa(mat%now%strn,mat%now%strn2D,dyn%now%ux,dyn%now%uy,dyn%now%uz, &
-        !                          tpo%now%H_ice,tpo%now%f_ice,tpo%now%f_grnd,mat%par%zeta_aa, &
-        !                          mat%par%zeta_ac,mat%par%dx,mat%par%de_max,mat%par%n_glen)
-
-end if 
 
         ! Assign vertical average of shear for external use 
         mat%now%f_shear_bar = mat%now%strn2D%f_shear

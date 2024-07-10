4fcd1d37bc7468f893d6db3166ff68c50087ca9b
Alexander Robinson <alexander.robinson@awi.de>
2024-07-10 09:58:54 +0200

    Retaining wp=sp.

--- src/yelmo_defs.f90
+++ src/yelmo_defs.f90
@@ -18,7 +18,7 @@ module yelmo_defs
     integer,  parameter :: sp  = kind(1.0)
 
     ! Choose the precision of the library (sp,dp)
-    integer,  parameter :: wp = dp 
+    integer,  parameter :: wp = sp 
 
     ! Define a yelmo-specific kind parameter so that
     ! external models can make use of the same precision

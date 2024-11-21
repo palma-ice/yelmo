4b2968552fbb00aa6f17e2c707dd3e57468e4801
Alexander Robinson <alexander.robinson@awi.de>
2024-11-21 06:46:49 +0100

    Minor change to unit test script.

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -16,3 +16,5 @@ echo "Comparing with: ${fldr_ref}"
 #./runylmo -r -e benchmarks -o ${fldr}/expa   -n par-gmd/yelmo_EISMINT_expa.nml   -p ctrl.dt2D_out=1000 ctrl.time_end=20000
 #./runylmo -r -e benchmarks -o ${fldr}/expf   -n par-gmd/yelmo_EISMINT_expf.nml   -p ctrl.dt2D_out=1000 ctrl.time_end=20000
 
+
+# Now call comparison script

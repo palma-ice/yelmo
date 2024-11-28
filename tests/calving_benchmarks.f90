c3b0cdf3ea0c3d474974ee89b4f4ef3fbec1733f
Alexander Robinson <alexander.robinson@awi.de>
2024-11-28 06:23:51 +0100

    calving benchmark: additional setup.

--- tests/calving_benchmarks.f90
+++ tests/calving_benchmarks.f90
@@ -1,9 +1,82 @@
 module calving_benchmarks
 
+    use yelmo_defs, only : wp
+
     implicit none
 
 contains
 
+    subroutine calvmip_init(z_bed,xx,yy,domain)
 
+        implicit none
+
+        real(wp), intent(OUT) :: z_bed(:,:)
+
+        real(wp), intent(IN)  :: xx(:,:)
+        real(wp), intent(IN)  :: yy(:,:)
+        character(len=*), intent(IN) :: domain
+        
+        ! Local variables
+        real(wp) :: R0, Bc, Bl, Ba, rc
+
+        select case(trim(domain))
+
+            case("circular")
+                ! R0=800e3;
+                ! Bc=900; 
+                ! Bl=-2000;   
+                ! rc=0;
+                
+                call define_bedrock_circular(z_bed,xx,yy,R0,Bc,Bl,rc)
+
+            ! case("thule")
+
+                ! R0=800e3;
+                ! Bc=900; 
+                ! Bl=-2000; 
+                ! Ba=1100;  
+                ! rc=0;
+
+                ! r=sqrt(xx*xx+yy*yy);
+                ! theta=atan2(yy,xx);
+                ! l=R-cos(2*theta).*R/2;
+                ! a=Bc-(Bc-Bl)*(r-rc).^2./(R-rc).^2;
+                ! z_bed = Ba*cos(3*pi*r / l)+a ;
+
+            case DEFAULT
+
+                write(*,*) "calvmip_init:: Error: domain not recognized."
+                write(*,*) "domain = ", trim(domain)
+                stop 
+
+        end select
+
+        return
+
+    end subroutine calvmip_init
+
+    elemental subroutine define_bedrock_circular(z_bed,xx,yy,R0,Bc,Bl,rc)
+
+        implicit none
+
+        real(wp), intent(OUT) :: z_bed
+        real(wp), intent(IN)  :: xx
+        real(wp), intent(IN)  :: yy
+        real(wp), intent(IN)  :: R0
+        real(wp), intent(IN)  :: Bc
+        real(wp), intent(IN)  :: Bl
+        real(wp), intent(IN)  :: rc
+        
+        ! Local variables
+        real(wp) :: r
+        real(wp) :: theta
+
+        r = sqrt(xx*xx+yy*yy);
+        theta = atan2(yy,xx);
+        z_bed = Bc-(Bc-Bl)*(r-rc)**2 / (R0-rc)**2;
+
+        return
+
+    end subroutine define_bedrock_circular
 
 end module calving_benchmarks
--- tests/yelmo_calving.f90
+++ tests/yelmo_calving.f90
@@ -8,6 +8,64 @@ program yelmo_calving
     
     implicit none
 
+    type(yelmo_class)     :: yelmo1
+    
+    type control_type
+        character(len=56)  :: domain    
+        character(len=256) :: outfldr
+        character(len=256) :: file2D, file1D
+        character(len=256) :: file_restart
+        character(len=512) :: path_par 
+        character(len=56)  :: exp
+
+        real(wp) :: time_init, time_end, time, dtt
+        real(wp) :: dt2D_out, dt1D_out
+
+        real(wp) :: dx
+    end type
+
+    type(control_type) :: ctl
+
+    
+    real(8) :: cpu_start_time, cpu_end_time, cpu_dtime  
+    
+    ! Start timing 
+    call yelmo_cpu_time(cpu_start_time)
+
+    ! Assume program is running from the output folder
+    ctl%outfldr = "./"
+
+    ! Determine the parameter file from the command line 
+    call yelmo_load_command_line_args(ctl%path_par)
+    !path_par   = trim(outfldr)//"yelmo_calving.nml" 
+
+    ! Define input and output locations 
+    ctl%file1D       = trim(ctl%outfldr)//"yelmo1D.nc"
+    ctl%file2D       = trim(ctl%outfldr)//"yelmo2D.nc"
+    ctl%file_restart = trim(ctl%outfldr)//"yelmo_restart.nc"
+
+    
+    ! Define the domain, grid and experiment from parameter file
+    call nml_read(ctl%path_par,"ctl","exp",         ctl%exp)            ! "exp1", "exp2", "exp3", "exp4", "exp5"
+    call nml_read(ctl%path_par,"ctl","dx",          ctl%dx)             ! [km] Grid resolution 
+
+    ! Timing parameters 
+    call nml_read(ctl%path_par,"ctl","time_init",   ctl%time_init)      ! [yr] Starting time
+    call nml_read(ctl%path_par,"ctl","time_end",    ctl%time_end)       ! [yr] Ending time
+    call nml_read(ctl%path_par,"ctl","dtt",         ctl%dtt)            ! [yr] Main loop time step 
+    call nml_read(ctl%path_par,"ctl","dt2D_out",    ctl%dt2D_out)       ! [yr] Frequency of 2D output 
+    ctl%dt1D_out = ctl%dtt  ! Set 1D output to frequency of main loop timestep 
+
+    ! Define domain based on experiment
+    select case(trim(ctl%exp))
+        case("exp1","exp2")
+            ctl%domain = "circular"
+        case("exp3","exp4","exp5")
+            ctl%domain = "thule"
+        case DEFAULT
+            write(*,*) "ctl.exp = ",trim(ctl%domain), " not recognized."
+            stop
+    end select
 
 
 

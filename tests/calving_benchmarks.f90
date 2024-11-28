d87ab4e4da36c6e2f062ced9c7cc83339e537c3e
Alexander Robinson <alexander.robinson@awi.de>
2024-11-28 06:51:46 +0100

    calving benchmarks: added thule domain too.

--- tests/calving_benchmarks.f90
+++ tests/calving_benchmarks.f90
@@ -1,6 +1,6 @@
 module calving_benchmarks
 
-    use yelmo_defs, only : wp
+    use yelmo_defs, only : wp, pi
 
     implicit none
 
@@ -23,26 +23,22 @@ contains
 
             case("circular")
                 
-                R0=800e3;
-                Bc=900; 
-                Bl=-2000;   
-                rc=0;
+                R0 = 800e3;
+                Bc = 900; 
+                Bl = -2000;   
+                rc = 0;
                 
                 call define_bedrock_circular(z_bed,xx,yy,R0,Bc,Bl,rc)
 
-            ! case("thule")
+            case("thule")
 
-                ! R0=800e3;
-                ! Bc=900; 
-                ! Bl=-2000; 
-                ! Ba=1100;  
-                ! rc=0;
+                R0 = 800e3;
+                Bc = 900; 
+                Bl = -2000; 
+                Ba = 1100;  
+                rc = 0;
 
-                ! r=sqrt(xx*xx+yy*yy);
-                ! theta=atan2(yy,xx);
-                ! l=R-cos(2*theta).*R/2;
-                ! a=Bc-(Bc-Bl)*(r-rc).^2./(R-rc).^2;
-                ! z_bed = Ba*cos(3*pi*r / l)+a ;
+                call define_bedrock_thule(z_bed,xx,yy,R0,Bc,Bl,Ba,rc)
 
             case DEFAULT
 
@@ -57,6 +53,8 @@ contains
     end subroutine calvmip_init
 
     elemental subroutine define_bedrock_circular(z_bed,xx,yy,R0,Bc,Bl,rc)
+        ! Details here:
+        ! https://github.com/JRowanJordan/CalvingMIP/wiki/circular-domain
 
         implicit none
 
@@ -79,5 +77,35 @@ contains
         return
 
     end subroutine define_bedrock_circular
+
+    elemental subroutine define_bedrock_thule(z_bed,xx,yy,R0,Bc,Bl,Ba,rc)
+        ! Details here: 
+        ! https://github.com/JRowanJordan/CalvingMIP/wiki/Thule-domain
+
+        implicit none
+
+        real(wp), intent(OUT) :: z_bed
+        real(wp), intent(IN)  :: xx
+        real(wp), intent(IN)  :: yy
+        real(wp), intent(IN)  :: R0
+        real(wp), intent(IN)  :: Bc
+        real(wp), intent(IN)  :: Bl
+        real(wp), intent(IN)  :: Ba
+        real(wp), intent(IN)  :: rc
+        
+        ! Local variables
+        real(wp) :: r
+        real(wp) :: theta
+        real(wp) :: l, a 
+
+        r = sqrt(xx*xx+yy*yy);
+        theta = atan2(yy,xx);
+        l = R0-cos(2*theta)*R0/2;
+        a = Bc-(Bc-Bl)*(r-rc)**2 / (R0-rc)**2;
+        z_bed = Ba*cos(3*pi*r / l)+a ;
+
+        return
+
+    end subroutine define_bedrock_thule
 
 end module calving_benchmarks

a759a208bbba4af13b856c4584df5a203c163bb9
Alexander Robinson <alexander.robinson@awi.de>
2026-03-13 15:45:09 +0100

    Yelmo.jl: yelmo_init works, and now added yelmo_get_grid_info to obtain grid dimensions and axes. Also yelmo_get_var2D or yelmo_get_var3D work for most variables now too.

--- julia/Yelmo.jl
+++ julia/Yelmo.jl
@@ -16,12 +16,68 @@ function yelmo_step(time::Float64)
     ccall((:yelmo_step, yelmolib), Cvoid, (Float64,), time)
 end
 
-function yelmo_get_H_ice(nx::Int, ny::Int)
-    H = Matrix{Float64}(undef, nx, ny)
-    ccall((:yelmo_get_H_ice, yelmolib), Cvoid,
-        (Ptr{Float64}, Int32, Int32),
-        H, Int32(nx), Int32(ny))
-    return H
+function yelmo_get_grid_info()
+
+    # Step 1: get sizes
+    nx    = Ref{Cint}(0)
+    ny    = Ref{Cint}(0)
+    nz_aa = Ref{Cint}(0)
+    nz_ac = Ref{Cint}(0)
+
+    ccall((:yelmo_get_grid_sizes, yelmolib), Cvoid,
+          (Ref{Cint}, Ref{Cint}, Ref{Cint}, Ref{Cint}),
+          nx, ny, nz_aa, nz_ac)
+
+    # Step 2: allocate buffers
+    xc      = Vector{Cdouble}(undef, nx[])
+    yc      = Vector{Cdouble}(undef, ny[])
+    zeta_aa = Vector{Cdouble}(undef, nz_aa[])
+    zeta_ac = Vector{Cdouble}(undef, nz_ac[])
+
+    # Step 3: fill buffers
+    ccall((:yelmo_get_grid_info, yelmolib), Cvoid,
+          (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}),
+          xc, yc, zeta_aa, zeta_ac)
+
+    return (
+        nx    = Int(nx[]),
+        ny    = Int(ny[]),
+        nz_aa = Int(nz_aa[]),
+        nz_ac = Int(nz_ac[]),
+        xc      = xc,
+        yc      = yc,
+        zeta_aa = zeta_aa,
+        zeta_ac = zeta_ac,
+    )
+
+end
+
+function yelmo_get_var2D!(v2D::Array{Float64,2}, name::String)
+    nx, ny = size(v2D)
+    ccall((:yelmo_get_var2D, yelmolib), Cvoid,
+        (Ptr{Float64}, Int32, Int32, Ptr{UInt8}),
+        v2D, Int32(nx), Int32(ny), name * "\0")
+    return v2D
+end
+
+function yelmo_get_var2D(nx::Int, ny::Int, name::String)
+    v2D = Matrix{Float64}(undef, nx, ny)
+    yelmo_get_var2D!(v2D, name)
+    return v2D
+end
+
+function yelmo_get_var3D!(v3D::Array{Float64,3}, name::String)
+    nx, ny, nz = size(v3D)
+    ccall((:yelmo_get_var3D, yelmolib), Cvoid,
+        (Ptr{Float64}, Int32, Int32, Int32, Ptr{UInt8}),
+        v3D, Int32(nx), Int32(ny), Int32(nz), name * "\0")
+    return v3D
+end
+
+function yelmo_get_var3D(nx::Int, ny::Int, nz::Int, name::String)
+    v3D = Array{Float64}(undef, nx, ny, nz)
+    yelmo_get_var3D!(v3D, name)
+    return v3D
 end
 
 function yelmo_set_bmb(bmb::Matrix{Float64})
--- julia/test_yelmo.jl
+++ julia/test_yelmo.jl
@@ -10,24 +10,28 @@ include("Yelmo.jl")
 # Initialize Yelmo
 yelmo_init("../par/yelmo_initmip.nml", "file", 0.0)
 
-nx = 106
-ny = 181
-H = yelmo_get_H_ice(nx, ny)
+grd = yelmo_get_grid_info()
+
+H = yelmo_get_var2D(grd.nx, grd.ny, "tpo_H_ice")
+zs = yelmo_get_var2D(grd.nx, grd.ny, "tpo_z_srf")
+T = yelmo_get_var3D(grd.nx, grd.ny, grd.nz_aa, "thrm_T_ice")
 
 heatmap(H)
 
 
+
+
 # Coupled time loop
-for t in 0.0:dt:t_end
-    # Pull fields from Yelmo
-    H = yelmo_get_H_ice(nx, ny)
+# for t in 0.0:dt:t_end
+#     # Pull fields from Yelmo
+#     H = yelmo_get_H_ice(nx, ny)
 
-    # Run your Julia-side models using H, etc.
-    #bmb = run_basal_model(H, ...)
+#     # Run your Julia-side models using H, etc.
+#     #bmb = run_basal_model(H, ...)
 
-    # Push fields back into Yelmo's boundary conditions
-    yelmo_set_bmb(bmb)
+#     # Push fields back into Yelmo's boundary conditions
+#     yelmo_set_bmb(bmb)
 
-    # Advance Yelmo one step
-    yelmo_step(t)
-end
+#     # Advance Yelmo one step
+#     yelmo_step(t)
+# end
